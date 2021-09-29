d2d24669c28a891ac53bc68c62eebec7f8046a11
Haodaynay <91599150+Haodaynay@users.noreply.github.com>
2021-09-29 17:24:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     question: 'like dark night và share nhé',
     btnReply: 'Gửi cho Hào đây',
     reply: 'okee anhhhh',
-    mess: 'dark night hay quas',
+    mess: 'v:',
     messDesc: 'sinh nhật vui vẻ.',
     btnAccept: 'nhớ share nha',
     messLink: 'https://github.com/haoday/Confess-nguyen' //link mess của các bạn. VD: https://m.me/nam.nodemy
