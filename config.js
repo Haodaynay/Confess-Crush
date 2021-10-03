ab9301faf5322c81293bc1c0a3be1ad778f7f077
quocnam01 <91599150+quocnam01@users.noreply.github.com>
2021-10-03 20:42:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Thảo Nguyên',
-    introDesc: `mừng đảng mừng xuân
-    chúc mừng sinh nhật Nguyên `,
+    introTitle: 'Phương Loan',
+    introDesc: `Chúc Mừng Sinh Nhật Loan `,
     btnIntro: 'party',
-    title: 'chúc mừng sinh nhật Thảo Nguyên',
-    desc: ' Thảo Nguyên rất đáng yêu đúng không v: ',
-    btnYes: ' không',
-    btnNo: ' đúng vậy ',
-    question: 'like dark night và share nhé',
+    title: 'chúc mừng sinh nhật Phương Loan',
+    desc: ' m vẫn nợ t 100 tỷ đúng không ',
+    btnYes: ' đúng vậy',
+    btnNo: ' không ',
+    question: ' trả đi nhá. hơi lâu rồi đấy',
     btnReply: 'Gửi',
-    reply: 'okee anhhhh',
-    mess: 'chủ shop',
-    messDesc: 'sinh nhật vui v ẻ.',
+    reply: ' vâng em xin loi. em trả anh ngay ạ',
+    mess: 'Phương Loan',
+    messDesc: 'sinh nhật vui vẻ
+     theo dõi dark night nhé.',
     btnAccept: 'end',
-    messLink: 'https://www.facebook.com/profile.php?id=100009973713795' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100030070723774' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
