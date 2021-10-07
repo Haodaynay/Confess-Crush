e01001ca7653ebca285d2f4c3c0b29b279f834ed
haoday <91599150+haoday2@users.noreply.github.com>
2021-10-07 14:47:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Phương Loan',
-    introDesc: `Chúc Mừng Sinh Nhật Loan `,
-    btnIntro: 'party',
-    title: 'chúc mừng sinh nhật Phương Loan',
-    desc: ' m vẫn nợ t 100 tỷ đúng không ',
-    btnYes: ' đúng vậy',
+    introTitle: 'yangho Gia Lai',
+    introDesc: `chào Vũ Hậu `,
+    btnIntro: 'tiếp theo',
+    title: ' dark night',
+    desc: ' bro có cảm thấy có lỗi với tôi khi cứ gửi mấy cái số đấy không?? ',
+    btnYes: ' có. em xin lỗi anh ạ',
     btnNo: ' không ',
-    question: ' trả đi nhá. hơi lâu rồi đấy',
+    question: ' xin lỗi nhanh lên',
     btnReply: 'Gửi',
-    reply: ' vâng em xin loi. em trả anh ngay ạ',
-    mess: 'Phương Loan',
-    messDesc: 'sinh nhật vui vẻ. theo dõi dark night nhé v:',
+    reply: ' em xin lỗi anh. lần sau em không dám nữa ạ',
+    mess: 'ừm không sao đâu em,
+    messDesc: ' theo dõi dark night nhé v:',
     btnAccept: 'end',
-    messLink: 'https://www.facebook.com/messages/t/100030070723774' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/hoangcam99' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
